38fa8e7fc436bcb3fca5fb4f90fc266ad7aa9833
Delgado Quijandria Pierre Alberto <pdelgadoq@sunat.gob.pe>
2020-02-10 13:01:54 -0500

    Add callback cascade

--- 2.NODEJS_ASYNCHRONOUS/weather-app/app.js
+++ 2.NODEJS_ASYNCHRONOUS/weather-app/app.js
@@ -2,18 +2,23 @@
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode');
 
-forecast('-12.094426', '-77.028988', (error, data) => {
-    if (!error) {
-        console.log(data);
-    } else {
-        console.log(error);
-    }
-})
+// Obtenemos la direccion del usuario
+const address = process.argv[2]
 
-geocode('Lima', (error, data) => {
-    if (!error) {
-        console.log(data);
-    } else {
-        console.log(error);
-    }
-})
+// Validamos si se da una direccion por el usuario
+if (!address) {
+    console.log('Please provide an Address');
+} else {
+    geocode(address, (error, geocodeData) => {
+        if (error) {
+            return console.log(error);
+        } 
+        forecast(geocodeData.latitude, geocodeData.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            } 
+            console.log(geocodeData.location);
+            console.log(forecastData);    
+        })
+    })
+}
