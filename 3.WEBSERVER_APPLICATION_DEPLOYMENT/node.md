a42bd7ce6507fb5109ac41cfe5369e385b369987
Delgado Quijandria Pierre Alberto <pdelgadoq@sunat.gob.pe>
2020-02-12 14:37:05 -0500

    Add serving static assets

---
+++ 3.WEBSERVER_APPLICATION_DEPLOYMENT/node.md
@@ -0,0 +1,9 @@
+
+<!-- 
+
+El directorio public contendra los archivos staticos que seran servidos por el servidor
+
+public/index.html
+Es un nombre especial que indica que va a ser el archivo que va a ser servido por defecto
+
+--!>
--- 3.WEBSERVER_APPLICATION_DEPLOYMENT/web-server/src/app.js
+++ 3.WEBSERVER_APPLICATION_DEPLOYMENT/web-server/src/app.js
@@ -1,8 +1,27 @@
-// Cargamos los modulos
+// Cargamos los mudulos de node
+// path es un modulo de node que nos ayuda en la manipulacion de rutas
+const path = require('path');
+// Cargamos los modulos de npm
 const express = require('express');
 
-// inicializamos la aplicacion wweb
+// inicializamos la aplicacion web
 const app = express()
+
+// Middleware
+// Metodo app.use es la forma de declarar un Middleware y esto se ejecuta al inicio del todo
+// Se ejecuta en el orden en que se coloca en el codigo
+// Recibe una funcion como parametro
+
+// express.static()
+// sirve para establecer la ruta publica donde se encuentran los archivos estaticos
+// Con esto express sirve todos los archivos estaticos dinamicamente y ya no tenemos que colocar un metodo app.get por cada ruta
+
+// __dirname indica la ruta de acceso al directorio que contiene nuestra aplicacion app.js
+// __filename indica la ruta de acceso al archivo mismo app.js
+// path ofrece el metodo join que nos permite manipular, avanzar y retrocer de un directorio a otro
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
 
 // Definimos los metodos
 // Establecemos un controlador para una peticion GET, es decir que va a suceder cuando realicen una peticion GET a una ruta especifica
@@ -15,28 +34,33 @@ const app = express()
 //  - 2do arg: es la respuesta (response)
 //  Contiene un monton de metodos, para que pueda responder a la solictud de la forma que quiera, puede personalizar los datos que envia devuelto
 
-app.get('', (req, res) => {
-    // metodo res.send permite enviar datos como respuesta
-    // Cuando alguien vea el sitio web, van a ver esta cadena si hacen una peticion desde una aplicacion o el navegador
-    // Estamos enviando una cadena desde el servidor al cliente que hizo la solicitud
-    // res.send('Hello express!')
 
-    // Para enviar html como respuesta
-    // res.send('<h1>Weather</h1>')
-    // Para enviar un objeto json 
-    // res.send({name: 'Pierre'})
-    // Para enviar un array de objetos json
-    res.send([{name: 'Pierre'}, {name: 'Natalie'}])
+// Comentamos este codigo, debido a que nunca va a funcionar debido a que la primera coincidencia del la ruta raiz va a encontrarla en el directorio publico de archivos estaticos, en el archivo index.html, este indica que se refiere a lo que va a servir por defecto al entrar a esa ruta raiz
 
-})
+// app.get('', (req, res) => {
+//     // metodo res.send permite enviar datos como respuesta
+//     // Cuando alguien vea el sitio web, van a ver esta cadena si hacen una peticion desde una aplicacion o el navegador
+//     // Estamos enviando una cadena desde el servidor al cliente que hizo la solicitud
+//     // res.send('Hello express!')
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
-});
+//     // Para enviar html como respuesta
+//     // res.send('<h1>Weather</h1>')
+//     // Para enviar un objeto json 
+//     // res.send({name: 'Pierre'})
+//     // Para enviar un array de objetos json
+//     res.send([{name: 'Pierre'}, {name: 'Natalie'}])
 
-app.get('/about', (req, res) => {
-   res.send('About page') 
-});
+// })
+
+// Comentamos este codigo porque en el directorio de staticos va a encontrar help.html y about.html respectivamente
+
+// app.get('/help', (req, res) => {
+//     res.send('Help page')
+// });
+
+// app.get('/about', (req, res) => {
+//    res.send('About page') 
+// });
 
 app.get('/weather', (req, res) => {
     res.send('Weather page') 	
